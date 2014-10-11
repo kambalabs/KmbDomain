e2d8dfb547a0cdaf61082879c7e6a970bfaeb9af
Emmanuel Bouton <goten4@gmail.com>
2014-10-11 23:58:33 +0200

    Add PuppetClassFactory

--- config/module.config.php
+++ config/module.config.php
@@ -1,3 +1,11 @@
 <?php
 return [
+    'service_manager' => [
+        'invokables' => [
+            'KmbDomain\Model\PuppetClassFactory' => 'KmbDomain\Model\PuppetClassFactory',
+        ],
+        'aliases' => [
+            'puppetClassFactory' => 'KmbDomain\Model\PuppetClassFactory',
+        ],
+    ],
 ];
