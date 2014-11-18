dee4ec9d7eb118a1996a0c371f323cb5390cec29
Emmanuel Bouton <goten4@gmail.com>
2014-11-18 11:46:22 +0100

    Move class templates hydrator to KmbPmProxy

--- config/module.config.php
+++ config/module.config.php
@@ -3,15 +3,9 @@ return [
     'service_manager' => [
         'invokables' => [
             'KmbDomain\Model\GroupParameterFactory' => 'KmbDomain\Model\GroupParameterFactory',
-            'KmbDomain\Model\ParameterTemplateHydrator' => 'KmbDomain\Model\ParameterTemplateHydrator',
-        ],
-        'factories' => [
-            'KmbDomain\Model\ClassTemplatesHydrator' => 'KmbDomain\Service\ClassTemplatesHydratorFactory',
         ],
         'aliases' => [
             'groupParameterFactory' => 'KmbDomain\Model\GroupParameterFactory',
-            'classTemplatesHydrator' => 'KmbDomain\Model\ClassTemplatesHydrator',
-            'parameterTemplateHydrator' => 'KmbDomain\Model\ParameterTemplateHydrator',
         ],
     ],
 ];
